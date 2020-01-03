9a383a9c9a5046ad92209da1cfe1660c07c97a18
Rajarshi Singh <rajarshi129@gmail.com>
2020-01-02 17:53:51 -0800

    r

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi there! Hello from inside! Are you surprised?!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white'
+  },
+  text: {
+    color: 'white'
+  },
+});

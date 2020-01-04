bee56fd6b9ac0cd0691205eb6fff9863286cab86
Rajarshi Singh <rajarshi129@gmail.com>
2020-01-03 16:51:19 -0800

    r

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi there! Hello from inside the app.</Text>
+      <Text style={styles.text}>Hello from inside the app!</Text>
     </View>
   );
 }

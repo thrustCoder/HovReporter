b1061cf29be4d90ab50992e0adae30c1d84618b7
Rajarshi Singh <rajarshi129@gmail.com>
2020-03-28 21:13:45 -0700

    Dummy data auto populate

--- App.tsx
+++ App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  let injectedJavaScript = `document.getElementById('edit-submitted-license').value = 'BEE4547'; ` +
+                           `document.getElementById('edit-submitted-state').value = 'AL'; ` +
+                           `document.getElementById('edit-submitted-highway-ferry-terminal').value = 'SR-16WB'; ` +
+                           `document.getElementById('edit-submitted-location').value = 'Driving on the highway HOV lane'`;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello from inside the React Native app!</Text>
+      <WebView source={{ uri: "https://www.wsdot.wa.gov/travel/highways-bridges/hov/report-violator" }} 
+               javaScriptEnabled={true} 
+               injectedJavaScript={injectedJavaScript}/>
     </View>
   );
+}
+
+async function onPress() {
+  Alert.alert('Pressed!');
+  console.log('Pressed!');
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white'
-  },
-  text: {
-    color: 'white'
-  },
+  }
 });

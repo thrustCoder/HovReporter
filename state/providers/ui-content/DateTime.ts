0e0ca0ad3de08c63b518f7d144441de324f54220
Rajarshi <rajarshi129@gmail.com>
2020-04-19 16:08:25 -0700

    All data providers (#12)

---
+++ state/providers/ui-content/DateTime.ts
@@ -0,0 +1,243 @@
+const monthPickerItems = [{ 
+        label: 'Jan',
+        value: '1'
+    }, { 
+        label: 'Feb', 
+        value: '2' 
+    }, { 
+        label: 'Mar', 
+        value: '3' 
+    }, { 
+        label: 'Apr', 
+        value: '4' 
+    }, { 
+        label: 'May', 
+        value: '5' 
+    }, { 
+        label: 'Jun', 
+        value: '6' 
+    }, { 
+        label: 'Jul', 
+        value: '7' 
+    }, { 
+        label: 'Aug', 
+        value: '8' 
+    }, { 
+        label: 'Sep', 
+        value: '9' 
+    }, { 
+        label: 'Oct', 
+        value: '10' 
+    }, { 
+        label: 'Nov', 
+        value: '11' 
+    }, { 
+        label: 'Dec', 
+        value: '12' 
+    },
+];
+
+const dayPickerItems = [{ 
+        label: '1', 
+        value: '1' 
+    }, { 
+        label: '2', 
+        value: '2' 
+    }, { 
+        label: '3', 
+        value: '3' 
+    }, { 
+        label: '4', 
+        value: '4' 
+    }, { 
+        label: '5', 
+        value: '5' 
+    }, { 
+        label: '6', 
+        value: '6' 
+    }, { 
+        label: '7', 
+        value: '7' 
+    }, { 
+        label: '8', 
+        value: '8' 
+    }, { 
+        label: '9', 
+        value: '9' 
+    }, { 
+        label: '10', 
+        value: '10' 
+    }, { 
+        label: '11', 
+        value: '11' 
+    }, { 
+        label: '12', 
+        value: '12' 
+    }, { 
+        label: '13', 
+        value: '13' 
+    }, { 
+        label: '14', 
+        value: '14' 
+    }, { 
+        label: '15', 
+        value: '15' 
+    }, { 
+        label: '16', 
+        value: '16' 
+    }, { 
+        label: '17', 
+        value: '17' 
+    }, { 
+        label: '18', 
+        value: '18' 
+    }, { 
+        label: '19', 
+        value: '19' 
+    }, { 
+        label: '20', 
+        value: '20' 
+    }, { 
+        label: '21', 
+        value: '21' 
+    }, { 
+        label: '22', 
+        value: '22' 
+    }, { 
+        label: '23', 
+        value: '23' 
+    }, { 
+        label: '24', 
+        value: '24' 
+    }, { 
+        label: '25', 
+        value: '25' 
+    }, { 
+        label: '26', 
+        value: '26' 
+    }, { 
+        label: '27', 
+        value: '27' 
+    }, { 
+        label: '28', 
+        value: '28' 
+    }, { 
+        label: '29', 
+        value: '29' 
+    }, { 
+        label: '30', 
+        value: '30' 
+    }, { 
+        label: '31', 
+        value: '31' 
+    },
+];
+
+const yearPickerItems = [{ 
+        label: '2020', 
+        value: '2020' 
+    }, { 
+        label: '2021', 
+        value: '2021' 
+    }, { 
+        label: '2022', 
+        value: '2022' 
+    }, { 
+        label: '2023', 
+        value: '2023' 
+    }, { 
+        label: '2024', 
+        value: '2024' 
+    }, { 
+        label: '2025', 
+        value: '2025' 
+    }
+];
+
+const hourPickerItems = [{ 
+        label: '1', 
+        value: '1' 
+    }, { 
+        label: '2', 
+        value: '2' 
+    }, { 
+        label: '3', 
+        value: '3' 
+    }, { 
+        label: '4', 
+        value: '4' 
+    }, { 
+        label: '5', 
+        value: '5' 
+    }, { 
+        label: '6', 
+        value: '6' 
+    }, { 
+        label: '7', 
+        value: '7' 
+    }, { 
+        label: '8', 
+        value: '8' 
+    }, { 
+        label: '9', 
+        value: '9' 
+    }, { 
+        label: '10', 
+        value: '10' 
+    }, { 
+        label: '11', 
+        value: '11' 
+    }, { 
+        label: '12', 
+        value: '12' 
+    }
+]
+
+const minutePickerItems = [{ 
+        label: '00', 
+        value: '0' 
+    }, { 
+        label: '05', 
+        value: '5' 
+    }, { 
+        label: '10', 
+        value: '10' 
+    }, { 
+        label: '15', 
+        value: '15' 
+    }, { 
+        label: '20', 
+        value: '20' 
+    }, { 
+        label: '25', 
+        value: '25' 
+    }, { 
+        label: '30', 
+        value: '30' 
+    }, { 
+        label: '35', 
+        value: '35' 
+    }, { 
+        label: '40', 
+        value: '40' 
+    }, { 
+        label: '45', 
+        value: '45' 
+    }, { 
+        label: '50', 
+        value: '50' 
+    }, { 
+        label: '55', 
+        value: '55' 
+    }
+];
+
+const datetime = {
+    monthPickerItems,
+    dayPickerItems,
+    yearPickerItems,
+    hourPickerItems,
+    minutePickerItems
+};
+
+export default datetime;

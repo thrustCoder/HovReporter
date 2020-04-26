6979e66fb8cd13ca204f5c816480f5f12ab42f89
Rajarshi Singh <rajarshi129@gmail.com>
2020-04-25 18:29:23 -0700

    Three screen beautify

---
+++ styles/Colors.ts
@@ -0,0 +1,6 @@
+const colors = {
+    red: '#FF6347',
+    green: '#32CD32'
+};
+
+export default colors;
--- views/DriverBlackhole.tsx
+++ views/DriverBlackhole.tsx
@@ -1,16 +1,53 @@
 import React, {Component} from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text, Button } from 'react-native-elements';
+import { Text, Icon, Image } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { updateOccupants } from '../state/actions/AppActions';
+import { clearAllState } from '../state/actions/AppActions';
+
+import colors from '../styles/Colors';
+import boundingLayout from '../styles/BoundingLayout';
+import contentItems from '../styles/ContentItems';
 
 class DriverBlackhole extends Component {
+    clearAllState() {
+        this.props.clearAllState();
+        this.props.navigation.popToTop();
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text h3>Please focus on driving.</Text>
-                <Text onPress={() => this.props.navigation.popToTop()}>Start over</Text>
+            <View style={boundingLayout.container}>
+                <View style={boundingLayout.header}>
+                    <View style={contentItems.cancelButton}>
+                        <Icon
+                            name='times-circle'
+                            type='font-awesome'
+                            color={colors.red}
+                            size='50'
+                            onPress={() => this.clearAllState()}
+                        />
+                    </View>       
+                </View>
+                <View style={boundingLayout.content}>
+                    <View style={boundingLayout.boundingContainer}>
+                        <View style={boundingLayout.topImageArea}>
+                            <Image style={contentItems.noTextWhileDriveImage} 
+                                source={require('../images/text-while-drive.png')} 
+                            />
+                        </View>
+                        <View style={boundingLayout.mainArea}>
+                            <Text h2 style={contentItems.mainText}>
+                                Please focus on driving.
+                            </Text>
+                            <Text h4 style={contentItems.mainText}>
+                                You can always report the violation later.
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={boundingLayout.footer}>
+                </View>
             </View>
         );
     }
@@ -31,12 +68,12 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (state) => {
-    const { appState } = state
-    return { appState }
+    const { appState, navState } = state
+    return { appState, navState }
 };
 const mapDispatchToProps = dispatch => (
     bindActionCreators({
-        updateOccupants,
+        clearAllState,
     }, dispatch)
 );
 

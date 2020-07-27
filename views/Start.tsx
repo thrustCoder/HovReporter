66bbbfe6e9cf9b19a792f6a08839c21e3c4e0723
Rajarshi Singh <rajarshi129@gmail.com>
2020-07-27 13:29:56 -0700

    upgrading app.json and Start.tsx

--- views/Start.tsx
+++ views/Start.tsx
@@ -39,7 +39,7 @@ class Start extends Component {
                 </View>
                 <View style={boundingLayout.footerVersion}>
                     <Text style={contentItems.versionText}>
-                        App version: v1.0.9
+                        App version: v1.1.0
                     </Text>
                 </View>
             </View>

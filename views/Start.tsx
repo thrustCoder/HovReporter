d4151dd041c74a89b3e97dd4f436fca8a91cc984
Rajarshi <rajarshi129@gmail.com>
2022-08-26 16:41:44 -0700

    r (#92)

--- views/Start.tsx
+++ views/Start.tsx
@@ -88,7 +88,7 @@ class Start extends Component {
                     <Text 
                         style={contentItems.versionText}
                         data-i9n-redact={true}>
-                        v3.0.0
+                        v3.0.1
                     </Text>
                 </View>
             </View>
